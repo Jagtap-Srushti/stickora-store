7661e1e13c24e6c491be52f929eb24d4949e0a8b
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-05-17 19:14:01 +0530

    Remove techHubBackend folder

--- techHubBackend/techHub/src/main/java/com/srushti/techHub/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.srushti.techHub.repository;
-
-import com.srushti.techHub.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
