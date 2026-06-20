5b94383b07170be72a9696d004940f15a20e283d
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-06-20 17:23:20 +0530

    Implement JPA auditing for entity tracking

--- Backend/stickora/src/main/java/com/srushti/stickora/entity/Product.java
+++ Backend/stickora/src/main/java/com/srushti/stickora/entity/Product.java
@@ -18,7 +18,7 @@ import java.time.Instant;
 @Component
 @Getter
 @Setter
-public class Product {
+public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,16 +39,6 @@ public class Product {
 
     @Column(name = "image_url")
     private String imageUrl;
-
-    @Column(name = "created_at",nullable = false)
-    private Instant createdAt;
-    @Column(name = "created_by",nullable = false)
-    private String createdBy;
-
-    @Column(name = "updated_at")
-    private Instant updatedAt;
-    @Column(name = "updated_by")
-    private String updatedBy;
 
 
 }
